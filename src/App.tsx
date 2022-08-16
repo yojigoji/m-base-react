a4a35ebd077d517160209ec488bf0650c951518c
yojigoji <gojie414@gmail.com>
2022-08-16 11:15:07 +0800

    chore: test pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App(): JSX.Element {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <div>test div</div>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />

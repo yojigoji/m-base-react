c52706bb5eae5e2a3bf4123f5003a14cfcad9427
yojigoji <gojie414@gmail.com>
2022-08-16 11:11:28 +0800

    chore: test pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App(): any {
+function App(): JSX.Element {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
